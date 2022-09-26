8e8a284a71cfa2d0ac42c440f506d7f84c8452c5
Miguel Guthridge <hdsq@outlook.com.au>
2022-09-27 00:05:36 +1000

    Fix flake8 error

--- backend/__init__.py
+++ backend/__init__.py
@@ -5,7 +5,6 @@ A next-gen forum to make life easier for educators and students alike.
 
 This is the main entrypoint to the program.
 """
-import sys
 from flask import Flask
 
 app = Flask(__name__)
