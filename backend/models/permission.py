e68d1ad8131a2465a0140a6a2ffb3f93aa2d88a8
Miguel Guthridge <hdsq@outlook.com.au>
2022-10-03 16:29:45 +1100

    Rename permissions to make usage easier

--- backend/models/permission.py
+++ backend/models/permission.py
@@ -11,38 +11,38 @@ class Permission(Enum):
     Enum containing definitions for all the permission types of users.
     """
     # Viewing
-    CanView = 0
+    View = 0
     """User can view public posts"""
-    CanViewPrivate = 1
+    ViewPrivate = 1
     """User can view private posts"""
 
     # Posting
-    CanPost = 10
+    Post = 10
     """User can create posts"""
-    CanAnswer = 11
+    Answer = 11
     """User can create answers to posts"""
 
     # Queues
-    CanDelegate = 20
+    Delegate = 20
     """User can move a post from the main queue to a specialised queue"""
-    CanFollowQueue = 21
+    FollowQueue = 21
     """User can subscribe to queues to get notifications when a post is moved
     into it"""
 
     # Moderation
-    CanReportPosts = 30
+    ReportPosts = 30
     """User can report posts to get them reviewed by a moderator"""
-    CanClosePosts = 31
+    ClosePosts = 31
     """User can close posts to give feedback before the post is reopened"""
-    CanDeletePosts = 32
+    DeletePosts = 32
     """User can delete posts to prevent them from being reopened"""
-    CanViewReports = 33
+    ViewReports = 33
     """User can view the list of reported posts"""
 
     # User management
-    CanSetUserPermission = 40
+    SetUserPermissions = 40
     """User can set the permission level of other users"""
-    CanAddUsers = 41
+    AddUsers = 41
     """User can create other user accounts"""
-    CanRemoveUsers = 42
+    RemoveUsers = 42
     """User can remove other user accounts"""
