b068861bf90e8e40bd7701a3b78e32a40330f436
Miguel Guthridge <hdsq@outlook.com.au>
2022-10-03 17:38:46 +1100

    Import permissions in models module

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -3,3 +3,10 @@
 
 Contains code for the data structures used internally by the backend.
 """
+from .permission import Permission
+from .permission_set import PermissionSet
+
+__all__ = [
+    'Permission',
+    'PermissionSet',
+]
