6efc0358c8ab4da6c115b4d6f97be5c73c1e9be8
Miguel Guthridge <hdsq@outlook.com.au>
2022-10-04 16:39:43 +1100

    Add SQLite and Piccolo support

---
+++ piccolo_conf.py
@@ -0,0 +1,10 @@
+from piccolo.conf.apps import AppRegistry
+from piccolo.engine.sqlite import SQLiteEngine
+
+
+DB = SQLiteEngine(path="project.sqlite")
+
+
+# A list of paths to piccolo apps
+# e.g. ['blog.piccolo_app']
+APP_REGISTRY = AppRegistry(apps=[])
