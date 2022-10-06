abc24c8cf3a3488d380c2281697f35f0b2c8aff7
Miguel Guthridge <hdsq@outlook.com.au>
2022-10-06 18:26:03 +1100

    Remove old tests since we know it's working at least

--- tests/integration/admin/users/register_test.py
+++ tests/integration/admin/users/register_test.py
@@ -12,48 +12,50 @@ Tests for bulk registering users
 * Existing usernames (none registered)
 * Existing emails (none registered)
 """
-from tests.integration.constructors import basic_permission_group
-from tests.integration.functions.admin import users
+# from tests.integration.constructors import basic_permission_group
+# from tests.integration.functions.admin import users
 
 
-def test_register_single_user():
-    """Can we register a single user"""
-    group = basic_permission_group()
-    reg = users.register(
-        [{
-            "name_first": "Henry",
-            "name_last": "VIII",
-            "username": "henry8",
-            "email": "henry@example.com",
-        }],
-        group,
-    )
-    all = users.all()["users"]
-    assert len(all) == 1
-    assert all[0]["user_id"] == reg["user_ids"][0]
+# Commented out because the API will change
 
-
-def test_register_multi_users():
-    """Can we register multiple users"""
-    group = basic_permission_group()
-    reg = users.register(
-        [
-            {
-                "name_first": "Henry",
-                "name_last": "VIII",
-                "username": "henry8",
-                "email": "henry8@example.com",
-            },
-            {
-                "name_first": "Henry",
-                "name_last": "IX",
-                "username": "henry9",
-                "email": "henry9@example.com",
-            },
-        ],
-        group,
-    )
-    all = users.all()["users"]
-    assert len(all) == 2
-    assert all[0]["user_id"] == reg["user_ids"][0]
-    assert all[1]["user_id"] == reg["user_ids"][1]
+# def test_register_single_user():
+#     """Can we register a single user"""
+#     group = basic_permission_group()
+#     reg = users.register(
+#         [{
+#             "name_first": "Henry",
+#             "name_last": "VIII",
+#             "username": "henry8",
+#             "email": "henry@example.com",
+#         }],
+#         group,
+#     )
+#     all = users.all()["users"]
+#     assert len(all) == 1
+#     assert all[0]["user_id"] == reg["user_ids"][0]
+#
+#
+# def test_register_multi_users():
+#     """Can we register multiple users"""
+#     group = basic_permission_group()
+#     reg = users.register(
+#         [
+#             {
+#                 "name_first": "Henry",
+#                 "name_last": "VIII",
+#                 "username": "henry8",
+#                 "email": "henry8@example.com",
+#             },
+#             {
+#                 "name_first": "Henry",
+#                 "name_last": "IX",
+#                 "username": "henry9",
+#                 "email": "henry9@example.com",
+#             },
+#         ],
+#         group,
+#     )
+#     all = users.all()["users"]
+#     assert len(all) == 2
+#     assert all[0]["user_id"] == reg["user_ids"][0]
+#     assert all[1]["user_id"] == reg["user_ids"][1]
