4d41f5207173eb19fb49cb2502e13132871b0725
Miguel Guthridge <hdsq@outlook.com.au>
2022-10-09 04:02:04 +1100

    Add login route

--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -3,8 +3,12 @@
 
 Authentication routes
 """
-from flask import Blueprint
-from backend.types.auth import IAuthInfo
+import json
+from flask import Blueprint, request
+from backend.models.user import User
+from backend.models.auth_config import AuthConfig
+from backend.util import http_errors
+from backend.types.auth import IAuthInfo, JWT
 
 
 auth = Blueprint('auth', 'auth')
@@ -23,6 +27,22 @@ def login() -> IAuthInfo:
     * `user_id`: `UserId`
     * `token`: `JWT`
     """
+    data = json.loads(request.data.decode('utf-8'))
+    username = data["username"]
+    password = data["password"]
+    AuthConfig().authenticate(username, password)
+    try:
+        u = User.from_username(username)
+    except http_errors.BadRequest:
+        raise http_errors.BadRequest(
+            "Although the username and password are correct, this user is not "
+            "registered with Ensemble. Please contact a forum administrator "
+            "to get your account registered."
+        ) from None
+    return {
+        "user_id": u.id,
+        "token": JWT(f"TODO - {u.id}",)
+    }
 
 
 @auth.post('/logout')
