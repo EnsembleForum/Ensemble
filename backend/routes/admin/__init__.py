66be3591a1a99dc6061965771ae40eb2486222e3
Miguel Guthridge <hdsq@outlook.com.au>
2022-10-08 18:13:02 +1100

    Minor improvements

--- backend/routes/admin/__init__.py
+++ backend/routes/admin/__init__.py
@@ -14,6 +14,11 @@ admin.register_blueprint(permissions, url_prefix='/permissions')
 admin.register_blueprint(users, url_prefix='/users')
 
 
+@admin.get('/is_first_run')
+def is_first_run():
+    """Returns whether the datastore is empty"""
+
+
 __all__ = [
     'admin',
 ]
