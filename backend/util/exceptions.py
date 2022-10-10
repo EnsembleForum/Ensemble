7fdd92f932be5d640d878d8a0dab285a1cbe2e02
Miguel Guthridge <hdsq@outlook.com.au>
2022-10-11 01:25:12 +1100

    Add improved exception types for models

---
+++ backend/util/exceptions.py
@@ -0,0 +1,40 @@
+"""
+# Backend / Util / Exceptions
+
+Exceptions used by the Ensemble backend - many of these inherit from
+http_errors, but can be used to indicate specific things that happened, so we
+don't accidentally catch exceptions we don't want to.
+"""
+from . import http_errors
+
+
+class IdNotFound(http_errors.BadRequest):
+    """
+    Couldn't find an entry with ID in the database.
+
+    Extends `BadRequest`
+    """
+
+
+class InvalidInput(http_errors.BadRequest):
+    """
+    An input was invalid (eg a text field that needed > 0 chars)
+
+    Extends `BadRequest`
+    """
+
+
+class PermissionError(http_errors.Forbidden):
+    """
+    User doesn't have permission to perform a certain action.
+
+    Extends `Forbidden`
+    """
+
+
+class AuthenticationError(http_errors.Forbidden):
+    """
+    User failed to authenticate (usually invalid token).
+
+    Extends `Forbidden`
+    """
