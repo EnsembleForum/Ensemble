1a5d64955024efba5e12d95a59284ebf1f67d00e
ndoot <z5311681@ad.unsw.edu.au>
2022-10-10 21:05:30 +1100

    Added routes for pages that may be necessary

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { SERVER_PATH } from './constants';
 import { requestOptions } from './interfaces';
+import BrowsePage from './pages/BrowsePage';
 import LoginPage from './pages/LoginPage';
+import PasswordResetPage from './pages/PasswordResetPage';
+import RegisterPage from './pages/RegisterPage';
+import TaskboardPage from './pages/TaskboardPage';
 
 export function ApiFetch (method : string, path : string, token : string, body? : string) {
   const requestOptions : requestOptions = {
@@ -36,6 +40,10 @@ function PassThrough () {
         <Routes>
           <Route path = "/" element={<Navigate to="/auth/login" />}></Route>
           <Route path='/auth/login' element={<LoginPage/>} />
+          <Route path='/auth/register' element={<RegisterPage/>} />
+          <Route path='/auth/password_reset' element={<PasswordResetPage/>} />
+          <Route path='/browse' element={<BrowsePage/>} />
+          <Route path='/taskboard' element={<TaskboardPage/>} />
         </Routes>
     </Router>
   );
---
+++ frontend/pages/BrowsePage.tsx
@@ -0,0 +1,12 @@
+import styled from "@emotion/styled";
+import React, { JSXElementConstructor, MouseEvent, ReactElement } from "react";
+
+interface Props {}
+
+const BrowsePage = (props: Props) => {
+  return (
+    <></>
+  );
+};
+
+export default BrowsePage;
