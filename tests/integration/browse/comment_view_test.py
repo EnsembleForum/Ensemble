5359d10baa56c097b5038855cb4ff88f64488b16
Yuk Liang Khor <75720742+kyl342000@users.noreply.github.com>
2022-10-21 23:27:19 +1100

    Modified comment_view_test to account for removal of me_too

--- tests/integration/browse/comment_view_test.py
+++ tests/integration/browse/comment_view_test.py
@@ -42,8 +42,7 @@ def test_get_comment_success(all_users: IAllUsers, make_posts: IMakePosts):
     assert comment["text"] == comment_text
     assert comment["replies"] == []
     assert isinstance(comment["timestamp"], int)
-    assert comment["reacts"]["me_too"] == 0
-    assert comment["reacts"]["thanks"] == 0
+    assert comment["thanks"] == 0
 
 
 def test_add_two_replies(all_users: IAllUsers, make_posts: IMakePosts):
