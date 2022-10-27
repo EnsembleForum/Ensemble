2ceff7fcf04277632c767ba5c793a8cfe8c03f37
Miguel Guthridge <hdsq@outlook.com.au>
2022-10-27 18:55:56 +1100

    Implement queue create tests

--- tests/integration/taskboard/post_list_test.py
+++ tests/integration/taskboard/post_list_test.py
@@ -4,10 +4,10 @@
 Tests for post list routes
 
 * Fails with invalid queue_id
-* Success with valid queue_id
+* No permission
+* Success
 """
 import pytest
-from typing import cast
 from backend.types.identifiers import QueueId
 from backend.util.http_errors import Forbidden, BadRequest
 from tests.integration.request.taskboard import (
@@ -23,22 +23,31 @@ from tests.integration.conftest import (
 
 def test_invalid_queue_id(
     basic_server_setup: IBasicServerSetup,
-    make_queues: IMakeQueues,
 ):
     """
     If we are given an invalid queue_id, we get a 400 error
     """
     token = basic_server_setup["token"]
-    invalid_queue_id = (
-        max(make_queues["queue1_id"], make_queues["queue2_id"]) + 1
-    )
-
-    invalid_queue_id = cast(QueueId, invalid_queue_id)
+    invalid_queue_id = QueueId(-1)
     with pytest.raises(BadRequest):
         post_list(token, invalid_queue_id)
 
 
-def test_get_queue_success(basic_server_setup: IBasicServerSetup):
+def test_no_permission(
+    all_users: IAllUsers,
+    make_queues: IMakeQueues,
+):
+    """
+    Users shouldn't be able to create queues
+    """
+    with pytest.raises(Forbidden):
+        post_list(all_users['users'][0]['token'], make_queues["queue1_id"])
+
+
+def test_success(
+    basic_server_setup: IBasicServerSetup,
+    make_queues: IMakeQueues,
+):
     """
     Testing that admins can successfully create queues
     """
@@ -47,25 +56,3 @@ def test_get_queue_success(basic_server_setup: IBasicServerSetup):
     queue_id = queue_create(token, "queue_name")["queue_id"]
     queue = post_list(token, queue_id)
     assert queue["queue_name"] == queue_name
-
-
-def test_queue_permissions_fail(all_users: IAllUsers):
-    """
-    Testing that when users try to create or delete queues it will fail
-    """
-    admin_token1 = all_users['admins'][0]['token']
-    admin_token2 = all_users['admins'][1]['token']
-    user_token1 = all_users['users'][0]['token']
-
-    queue1 = queue_create(admin_token1, "queue1_name")
-    queue2 = queue_create(admin_token2, "queue2_name")
-
-    queue1_id = queue1["queue_id"]
-    queue2_id = queue2["queue_id"]
-
-    assert queue1_id == 1
-    assert queue2_id == 2
-
-    # Should raise an error but currently does not
-    with pytest.raises(Forbidden):
-        queue_create(user_token1, "queue3_name")
---
+++ tests/integration/taskboard/queue_create_test.py
@@ -0,0 +1,44 @@
+"""
+# Tests / Integration / Taskboard / Queue create
+
+Tests for creating queues
+
+* No permission
+* Empty name
+* Success
+"""
+import pytest
+from backend.util.http_errors import Forbidden, BadRequest
+from tests.integration.request.taskboard import (
+    queue_create,
+    post_list,
+)
+from tests.integration.conftest import (
+    IAllUsers,
+    IBasicServerSetup,
+)
+
+
+def test_no_permission(all_users: IAllUsers):
+    """
+    Testing that when users try to create or delete queues it will fail
+    """
+    with pytest.raises(Forbidden):
+        queue_create(all_users['users'][0]['token'], "queue3_name")
+
+
+def test_empty_name(basic_server_setup: IBasicServerSetup):
+    """
+    Do we get a bad request if the queue name is empty
+    """
+    with pytest.raises(BadRequest):
+        queue_create(basic_server_setup['token'], "")
+
+
+def test_success(basic_server_setup: IBasicServerSetup):
+    """
+    Can we create a queue
+    """
+    queue = queue_create(basic_server_setup['token'], "my queue")
+    details = post_list(basic_server_setup['token'], queue['queue_id'])
+    assert details['queue_name'] == "my queue"
