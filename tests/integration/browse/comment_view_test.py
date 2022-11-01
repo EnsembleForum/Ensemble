0e0567b4641e2de5c3a236f72f4ccd7e322f8f7c
Yuk Liang Khor <75720742+kyl342000@users.noreply.github.com>
2022-11-01 22:09:22 +1100

    Fixed failing test

--- tests/integration/browse/comment_view_test.py
+++ tests/integration/browse/comment_view_test.py
@@ -48,7 +48,7 @@ def test_get_comment_success(
     assert comment["text"] == comment_text
     assert comment["replies"] == []
     assert isinstance(comment["timestamp"], int)
-    assert comment["thanks"] == 0
+    assert comment["thanks"] == []
 
 
 def test_add_two_replies(
