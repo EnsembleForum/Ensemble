5dd3efa5cce009daa01a12461e2967ec4d12c0d6
Miguel Guthridge <hdsq@outlook.com.au>
2022-11-13 06:52:45 +1100

    Implement tests for editing own profile without permissions

--- tests/integration/user/profile_edit_test.py
+++ tests/integration/user/profile_edit_test.py
@@ -7,11 +7,13 @@ Tests for editing a user profile
 * Mod can't edit profile (for all profile edit routes)
 * Admin can edit profile (for all profile edit routes)
 * Can't edit invalid profile (for all profile edit routes)
+* Can't edit own profile if no permission (for all profile edit routes)
 * Can set pronouns to None
 """
 
 from typing import Any, Callable, cast
 import pytest
+from backend.models.permissions.permission import Permission
 from backend.util import http_errors
 from backend.types.auth import JWT
 from backend.types.identifiers import UserId
@@ -22,9 +24,11 @@ from ensemble_request.user import (
     profile_edit_email,
     profile_edit_pronouns,
 )
+from ensemble_request.admin import permissions
 from ..conftest import (
     ISimpleUsers,
     IBasicServerSetup,
+    IPermissionGroups,
 )
 
 
@@ -32,15 +36,18 @@ ProfileEditCallback = Callable[[JWT, UserId, str], None]
 """Type annotation for profile edit functions"""
 
 
-@pytest.mark.parametrize(
+test_params = (
     ('callback', 'new_key', 'new_value'),
     [
         (profile_edit_name_first, 'name_first', 'Robin'),
         (profile_edit_name_last, 'name_last', 'Banks'),
         (profile_edit_email, 'email', 'robin.banks@bigpond.com.au'),
         (profile_edit_pronouns, 'pronouns', 'He/him'),
-    ]
+    ],
 )
+
+
+@pytest.mark.parametrize(*test_params)
 def test_user_can_edit_own(
     callback: ProfileEditCallback,
     new_key: str,
@@ -62,17 +69,11 @@ def test_user_can_edit_own(
     assert p == profile(token, user_id)
 
 
-@pytest.mark.parametrize(
-    'callback',
-    [
-        profile_edit_name_first,
-        profile_edit_name_last,
-        profile_edit_email,
-        profile_edit_pronouns,
-    ]
-)
+@pytest.mark.parametrize(*test_params)
 def test_user_cant_edit_others(
     callback: ProfileEditCallback,
+    new_key: str,
+    new_value: str,
     simple_users: ISimpleUsers,
 ):
     """
@@ -82,19 +83,11 @@ def test_user_cant_edit_others(
         callback(
             simple_users["mod"]["token"],
             simple_users["user"]["user_id"],
-            "not allowed",
+            new_value,
         )
 
 
-@pytest.mark.parametrize(
-    ('callback', 'new_key', 'new_value'),
-    [
-        (profile_edit_name_first, 'name_first', 'Robin'),
-        (profile_edit_name_last, 'name_last', 'Banks'),
-        (profile_edit_email, 'email', 'robin.banks@bigpond.com.au'),
-        (profile_edit_pronouns, 'pronouns', 'He/him'),
-    ]
-)
+@pytest.mark.parametrize(*test_params)
 def test_admin_can_edit_others(
     callback: ProfileEditCallback,
     new_key: str,
@@ -116,17 +109,11 @@ def test_admin_can_edit_others(
     assert p == profile(token, user_id)
 
 
-@pytest.mark.parametrize(
-    'callback',
-    [
-        profile_edit_name_first,
-        profile_edit_name_last,
-        profile_edit_email,
-        profile_edit_pronouns,
-    ]
-)
+@pytest.mark.parametrize(*test_params)
 def test_cant_edit_invalid_profile(
     callback: ProfileEditCallback,
+    new_key: str,
+    new_value: str,
     basic_server_setup: IBasicServerSetup,
 ):
     """
@@ -136,7 +123,40 @@ def test_cant_edit_invalid_profile(
         callback(
             basic_server_setup["token"],
             UserId(-1),
-            "bad id",
+            new_value,
+        )
+
+
+@pytest.mark.parametrize(*test_params)
+def test_cant_edit_own_profile_without_permission(
+    callback: ProfileEditCallback,
+    new_key: str,
+    new_value: str,
+    simple_users: ISimpleUsers,
+    permission_groups: IPermissionGroups,
+):
+    """
+    We should get an error if we try to edit a profile that doesn't exist
+    """
+    # Disable users editing their own profile
+    permissions.groups_edit(
+        simple_users["admin"]["token"],
+        permission_groups["user"]["group_id"],
+        "User",
+        [
+            (
+                # Change EditProfile to False
+                p  # type: ignore
+                if p["permission_id"] != Permission.EditProfile.value
+                else p | {"value": False})
+            for p in permission_groups["user"]["permissions"]
+        ]
+    )
+    with pytest.raises(http_errors.Forbidden):
+        callback(
+            simple_users["user"]["token"],
+            simple_users["user"]["user_id"],
+            new_value,
         )
 
 
