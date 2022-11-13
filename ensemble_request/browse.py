64fcff750a45f221df6e59308e234ee165eeaaf8
Yuk Liang Khor <75720742+kyl342000@users.noreply.github.com>
2022-11-13 18:11:44 +1100

    Fixed failing tests

--- ensemble_request/browse.py
+++ ensemble_request/browse.py
@@ -27,7 +27,7 @@ from .helpers import post, get, put, delete
 URL = f"{URL}/browse"
 
 
-def post_list(token: JWT, search_term: str = None) -> IPostBasicInfoList:
+def post_list(token: JWT, search_term: str = "") -> IPostBasicInfoList:
     """
     ## GET `/browse/post_list`
 
@@ -40,7 +40,8 @@ def post_list(token: JWT, search_term: str = None) -> IPostBasicInfoList:
     * `Authorization` (`str`): JWT of the user
 
     ## Params
-    * `search_term` (`Optional[str]`): search term used for searching
+    * `search_term` (`str`): search term used for searching
+                             should be empty string if not searching
 
     ## Returns
     Object containing:
