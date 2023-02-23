281bcc97f1edc5b7f8909706fc440274d77003b7
Miguel Guthridge <hdsq@outlook.com.au>
2023-02-23 17:17:54 +1100

    Make backend run in deployment mode in run all script

--- scripts/run_all.py
+++ scripts/run_all.py
@@ -5,9 +5,18 @@ Run the backend and frontend servers
 """
 from _helpers import backend, mock_auth, frontend
 
+backend_url = None
+debug = False
+for var in sys.argv:
+    if var.startswith("--backend="):
+        backend_url = var.removeprefix("--backend=").strip()
+    elif var == "--debug":
+        debug = True
+
+
 auth = mock_auth()
-flask = backend(live_output=True, debug=True)
-npm = frontend()
+flask = backend(live_output=True, debug=debug)
+npm = frontend(backend_url)
 
 try:
     flask.wait()
